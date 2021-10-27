9435c2b1bb1f33024418d69d2304f9ae95da764f
AVO777 <seo@ox2.ru>
2021-10-28 01:01:07 +0300

    Домашнее задание к уроку №4

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'authapp',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'geekshop.wsgi.application'
+
+AUTH_USER_MODEL = 'authapp.ShopUser'
 
 
 # Database
