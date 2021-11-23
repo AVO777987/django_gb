9282577a49adc2450c8584c97f4ec2fe80ca7372
AVO777 <seo@ox2.ru>
2021-11-24 00:03:38 +0300

    Домашнее задание к уроку №10

--- geekshop/adminapp/forms.py
+++ geekshop/adminapp/forms.py
@@ -23,3 +23,4 @@ class ProductEditForm(forms.ModelForm):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
             field.help_text = ''
+
