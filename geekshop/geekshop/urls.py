0b1f9757508ef2714cd554235ca4448e907f4929
AVO777 <seo@ox2.ru>
2021-11-27 17:19:38 +0300

    Домашнее задание к уроку №12

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('contact/', mainapp.contact, name='contact'),
     path('products/', include('mainapp.urls', namespace='products')),
     path('auth/', include('authapp.urls', namespace='auth')),
+    path('order/', include('ordersapp.urls', namespace='order')),
     path('basket/', include('basketapp.urls', namespace='basket')),
     path('admin/', include('adminapp.urls', namespace='adminapp')),
 
