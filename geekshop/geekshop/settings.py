9a6dc305f0a84e4ed59688f090ea5eb5fc3a8215
AVO777 <seo@ox2.ru>
2021-12-09 18:28:01 +0300

    Домашнее задание к уроку №13

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -90,10 +90,18 @@ AUTH_USER_MODEL = 'authapp.ShopUser'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geekshop',
+        'USER': 'postgres'
     }
 }
 
